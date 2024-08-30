82c14bb10595810e22ba0f49a2675af2fafd00e7
Diego Andres <diegoechalarp@hotmail.com>
2024-08-29 20:00:44 -0400

    first commit

---
+++ src/main/java/com/magic_trading_card/MagicTradingCardApplication.java
@@ -0,0 +1,13 @@
+package com.magic_trading_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicTradingCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicTradingCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magic_trading_card/MagicTradingCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magic_trading_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicTradingCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
